1c2cc4903ab9d0b43eb2ea356b1b6ffbf7feebca
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-10-26 17:50:01 -0500

    Adding routes for logged-in user

--- src/App.js
+++ src/App.js
@@ -6,21 +6,41 @@ import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import FlashProvider from './contexts/FlashProvider';
+import UserProvider from './contexts/UserProvider';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 export default function App() {
   return (
     <Container fluid className="App">
       <BrowserRouter>
-        <ApiProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<FeedPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/user/:username" element={<UserPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        </ApiProvider>
+        <FlashProvider>
+          <ApiProvider>
+            <UserProvider>
+              <Header />
+              <Routes>
+                <Route path="/login" element={
+                  <PublicRoute><LoginPage /></PublicRoute>
+                } />
+                <Route path="/register" element={
+                  <PublicRoute><RegistrationPage /></PublicRoute>
+                } />
+                <Route path="*" element={
+                  <PrivateRoute>
+                    <Routes>
+                      <Route path="/" element={<FeedPage />} />
+                      <Route path="/explore" element={<ExplorePage />} />
+                      <Route path="/user/:username" element={<UserPage />} />
+                      <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                  </PrivateRoute>
+                } />
+              </Routes>
+            </UserProvider>
+          </ApiProvider>
+        </FlashProvider>
       </BrowserRouter>
     </Container>
   );
