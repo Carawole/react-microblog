32f4b1a5c37dedbdeac8d2467319763f04af9ff8
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-10-29 14:42:53 -0500

    Adding post and login features

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import FlashProvider from './contexts/FlashProvider';
 import UserProvider from './contexts/UserProvider';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import EditUserPage from './pages/EditUserPage';
+import ChangePasswordPage from './pages/ChangePasswordPage';
+import ResetRequestPage from './pages/ResetRequestPage';
+import ResetPage from './pages/ResetPage';
+
 
 export default function App() {
   return (
@@ -27,12 +32,20 @@ export default function App() {
                 <Route path="/register" element={
                   <PublicRoute><RegistrationPage /></PublicRoute>
                 } />
+                 <Route path="/reset-request" element={
+                  <PublicRoute><ResetRequestPage /></PublicRoute>
+                } />
+                <Route path="/reset" element={
+                  <PublicRoute><ResetPage /></PublicRoute>
+                } />
                 <Route path="*" element={
                   <PrivateRoute>
                     <Routes>
                       <Route path="/" element={<FeedPage />} />
                       <Route path="/explore" element={<ExplorePage />} />
                       <Route path="/user/:username" element={<UserPage />} />
+                      <Route path="/edit" element={<EditUserPage />} />
+                      <Route path="/password" element={<ChangePasswordPage />} />
                       <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
                   </PrivateRoute>
